2d39f5d775e25a2705c1e0eccc7bb4ba29f830f0
tenyon61 <tenyon@cqbo.com>
2025-05-24 12:33:27 +0800

    build: 升级minio，sa-token

--- src/main/java/com/tenyon/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tenyon/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.tenyon.common.exception;
 
 import com.tenyon.common.domain.vo.resp.RtnData;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 /**
  * 全局异常处理器
  */
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
